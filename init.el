b3ebc7acb09be9ddcb54998805aa273d6715bed1
Joshua Moller-Mara <j.moller-mara@berkeley.edu>
2015-06-19 14:44:33 -0700

    Added by package.el

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/org") ;Newer version of org mode
 (require 'org)
 (org-babel-load-file "~/.emacs.d/jmm-emacs.org")
