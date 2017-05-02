ffa7196ae436fd89d080aa80e8026d9e5f01cbf7
Josh Moller-Mara <j.mollermara@nyu.edu>
2017-05-02 11:10:52 -0400

    Some .emacs speed optimizations

--- init.el
+++ init.el
@@ -8,3 +8,6 @@
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/org") ;Newer version of org mode
 (require 'org)
 (org-babel-load-file "~/.emacs.d/jmm-emacs.org")
+;; (org-babel-tangle-file "~/.emacs.d/jmm-emacs.org" "~/.emacs.d/jmm-emacs.el")
+;; (byte-compile-file "~/.emacs.d/jmm-emacs.el")
+;; (load-file "~/.emacs.d/jmm-emacs.elc")
