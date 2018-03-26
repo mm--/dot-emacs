293d6593ac04d30c747f2619eb81710a85aa0bfb
Josh Moller-Mara <jmm@cns.nyu.edu>
2018-03-26 11:37:22 +0800

    Load org only when needed

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -5,12 +9,24 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/org") ;Newer version of org mode
-(require 'org)
-(org-babel-load-file "~/.emacs.d/jmm-emacs.org")
-(org-babel-load-file "~/.emacs.d/jmm-org-config.org")
+;; (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/org") ;Newer version of org mode
+;; (require 'org)
+;; (org-babel-load-file "~/.emacs.d/jmm-emacs.org")
+;; (org-babel-load-file "~/.emacs.d/jmm-org-config.org")
 ;; (org-babel-tangle-file "~/.emacs.d/jmm-emacs.org" "~/.emacs.d/jmm-emacs.el")
 ;; (byte-compile-file "~/.emacs.d/jmm-emacs.el")
 ;; (load-file "~/.emacs.d/jmm-emacs.elc")
 
-(jmm/load-personal-files)
+
+
+(if (or (file-newer-than-file-p "~/.emacs.d/jmm-emacs.org" "~/.emacs.d/jmm-emacs.el")
+	(file-newer-than-file-p "~/.emacs.d/jmm-org-config.org" "~/.emacs.d/jmm-org-config.el"))
+    (progn
+      (require 'org)
+      (org-babel-load-file "~/.emacs.d/jmm-emacs.org")
+      (org-babel-load-file "~/.emacs.d/jmm-org-config.org"))
+  (load-file "~/.emacs.d/jmm-emacs.el")
+  (eval-after-load 'org
+    '(progn (load-file "~/.emacs.d/jmm-org-config.el"))))
+
+;; (jmm/load-personal-files)
