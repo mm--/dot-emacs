ee7eb1232b325f537ed9e4b0da787782a5f335a0
Josh Moller-Mara <j.mollermara@nyu.edu>
2018-04-26 14:05:54 +0800

    Some documentation for init.el

--- init.el
+++ init.el
@@ -1,16 +1,16 @@
+;; Bootstrap initialization stuff
+
 ;; Speed up load time by preventing GC pauses
 (setq gc-cons-threshold 64000000)
 (add-hook 'after-init-hook #'(lambda ()
                                ;; restore after startup
                                (setq gc-cons-threshold 800000)))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Package-initialize is automatically called in Emacs 27
 (unless (>= emacs-major-version 27)
   (package-initialize))
 
+;; These comments are here for me to manually run with C-x C-e
 ;; (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/org") ;Newer version of org mode
 ;; (require 'org)
 ;; (org-babel-load-file "~/.emacs.d/jmm-emacs.org")
@@ -20,7 +20,10 @@
 ;; (byte-compile-file "~/.emacs.d/jmm-emacs.el")
 ;; (load-file "~/.emacs.d/jmm-emacs.elc")
 
-
+;; Don't load my org-config automatically, as it slows startup time
+;; It'll be automatically loaded when opening org files.
+;; However, we *do* need it if there are changes in any of the
+;; configs, since we'll need to re-tangle them.
 
 (if (or (file-newer-than-file-p "~/.emacs.d/jmm-emacs.org" "~/.emacs.d/jmm-emacs.el")
 	(file-newer-than-file-p "~/.emacs.d/jmm-org-config.org" "~/.emacs.d/jmm-org-config.el"))
