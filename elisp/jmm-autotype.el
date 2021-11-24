6c73c8aeb7eb98cbf3f091f2f5ac561ffb93012d
Josh Moller-Mara <jmm@cns.nyu.edu>
2021-11-24 12:51:47 -0800

    An example of using auto-insert for direnv files

---
+++ elisp/jmm-autotype.el
@@ -0,0 +1,13 @@
+(setq auto-insert-directory (locate-user-emacs-file "auto-insert/"))
+
+;; I put this before the auto-insert-alist because 'auto-insert-alist
+;; isn't immediately defined.
+(auto-insert-mode t)
+
+;; TODO: What's the proper wayt to add to variables not yet defined?
+;; Do I make an autoload declaration for it? Or do I use customize?
+(add-to-list 'auto-insert-alist
+	     '(("\\.envrc\\'" . "direnv envrc")
+	       . "envrc"))
+
+(provide 'jmm-autotype)
