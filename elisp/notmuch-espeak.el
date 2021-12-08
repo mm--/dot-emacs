752fa421d60f580d1de4ce2909ca3b5f00b945d5
Josh Moller-Mara <jmm@cns.nyu.edu>
2021-12-08 11:30:54 -0800

    Notmuch/espeak: Don't read out links in TTS

--- elisp/notmuch-espeak.el
+++ elisp/notmuch-espeak.el
@@ -15,8 +15,13 @@
 	(buffer-substring-no-properties (point-min) (point-max))
 	))))
 
-;; TODO: Wash out URLs, so I don't espeak them.
+(defun jmm/notmuch-show-get-text-no-links ()
+  (let ((notmuch-show-insert-text/plain-hook (append notmuch-show-insert-text/plain-hook '(jmm/notmuch-wash-remove-links))))
+    (jmm/notmuch-show-get-text)))
+
+;; DONE: Wash out URLs, so I don't espeak them.
 ;; TODO: Add some kind of hook to kill the process if the buffer is killed/buried
+;;       (Maybe associate the process with the buffer itself)
 
 ;;;###autoload
 (defun jmm/notmuch-show-espeak (&optional arg)
@@ -25,10 +30,10 @@
   (let ((process-old (get-buffer-process " *espeak-notmuch*")))
     (if (process-live-p process-old)
 	(kill-process process-old)
-	(let* ((wpm (number-to-string (* (if arg arg 5) 100)))
-               (process (start-process "espeak-process" " *espeak-notmuch*" "espeak" "-a" "200" "-s" wpm "--stdin")))
-	  (process-send-string process (jmm/notmuch-show-get-text))
-	  (process-send-string process "\n")
-	  (process-send-eof process)))))
+      (let* ((wpm (number-to-string (* (if arg arg 5) 100)))
+             (process (start-process "espeak-process" " *espeak-notmuch*" "espeak" "-a" "200" "-s" wpm "--stdin")))
+	(process-send-string process (jmm/notmuch-show-get-text-no-links))
+	(process-send-string process "\n")
+	(process-send-eof process)))))
 
 (provide 'notmuch-espeak)
